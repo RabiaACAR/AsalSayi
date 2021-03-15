217a918e8281989dd1273990ce62d67e8ec20ced
Rabia Acar <rabiaaaacar@hotmail.com>
2021-03-15 19:16:25 +0300

    Proje dosyası ekle.

---
+++ AsalSayi/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace AsalSayi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi;
+            Console.Write("Bir sayı giriniz : ");
+            sayi = Convert.ToInt32(Console.ReadLine());
+            AsalMi(sayi);
+
+            static void AsalMi(int sayi)
+            {
+                int value = 0;
+                for (int i = 2; i<sayi; i++)
+                {
+                   if (sayi%i==0)
+                    {
+                        value += 1;
+                    }
+                }
+                if (value>0)
+                {
+                    Console.WriteLine(sayi +" "+ " Asal sayı değildir.");
+                }
+                else
+                {
+                        Console.WriteLine(sayi+ " " + "Asal sayıdır.");
+                }
+                
+                
+            }
+        }
+    }
+}
